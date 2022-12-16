adec4ca2de220f49681687db2609771611d30bab
sgbaird <sterling.baird@utah.edu>
2022-12-15 22:57:26 -0700

    Update run_executable.py

--- scripts/particle_packing/run_executable.py
+++ scripts/particle_packing/run_executable.py
@@ -2,5 +2,9 @@ from os import getcwd
 from subprocess import run
 from os.path import join
 
-fpath = join("..", "..", "boppf", "utils", "particle_packing_sim.exe")
+print(getcwd())
+# fpath = join("..", "..", "boppf", "utils", "particle_packing_sim.exe")
+fpath = join(
+    "src", "matsci_opt_benchmarks", "particle_packing", "utils", "PackingGeneration.exe"
+)
 run(fpath)
