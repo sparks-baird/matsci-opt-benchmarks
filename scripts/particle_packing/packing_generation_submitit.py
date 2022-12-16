9409439eba9361b2d3851ebce8a0816fb495d7c9
Sterling Baird <sterling.baird@icloud.com>
2022-12-16 12:27:42 -0700

    record additional info for mongodb

--- scripts/particle_packing/packing_generation_submitit.py
+++ scripts/particle_packing/packing_generation_submitit.py
@@ -1,4 +1,5 @@
 # %% imports
+from time import time
 from submitit import AutoExecutor
 from os import path
 from datetime import datetime
@@ -19,14 +20,22 @@ from uuid import uuid4
 
 from matsci_opt_benchmarks.particle_packing.utils.packing_generation import (
     evaluate,
+    evaluate_batch,
     # collect_results,
 )
 from matsci_opt_benchmarks.particle_packing.utils.data import get_parameters
 
-dummy = True
+dummy = False
+SEED = 10
+if dummy:
+    num_samples = 2**3  # 2**3 == 8
+    num_repeats = 2
+else:
+    num_samples = 2**16  # 2**16 == 65536
+    num_repeats = 15
+
 slurm_savepath = path.join("data", "processed", "packing-generation-results.csv")
 job_pkl_path = path.join("data", "interim", "packing-generation-jobs.pkl")
-num_repeats = 10
 
 session_id = str(uuid4())
 
@@ -40,6 +49,8 @@ session_id = str(uuid4())
     orig_std_names,
 ) = get_parameters(remove_composition_degeneracy=True, remove_scaling_degeneracy=True)
 
+parameters.append({"name": "num_particles", "type": "range", "bounds": [100, 1000]})
+parameters.append({"name": "safety_factor", "type": "range", "bounds": [1.0, 2.5]})
 ax_client = AxClient()
 ax_client.create_experiment(
     name="boppf_sobol",
@@ -49,10 +60,10 @@ ax_client.create_experiment(
     parameter_constraints=["std1 <= std2", "comp1 + comp2 <= 1.0"],
 )
 search_space = ax_client.experiment.search_space
-m = get_sobol(search_space, fallback_to_sample_polytope=True)
-gr = m.gen(n=2**14)  # 2**14 == 16384
+m = get_sobol(search_space, fallback_to_sample_polytope=True, seed=SEED)
+gr = m.gen(n=num_samples)
 param_df = gr.param_df.copy()
-param_df["num_particles"] = 100
+# param_df["num_particles"] = 1000
 param_df["util_dir"] = path.join(
     "src", "matsci_opt_benchmarks", "particle_packing", "utils"
 )
@@ -65,19 +76,27 @@ if dummy:
     parameter_sets = parameter_sets[:10]
     batch_size = 5
 else:
-    batch_size = 100
+    batch_size = 700
 
 url = "https://data.mongodb-api.com/app/data-plyju/endpoint/data/v1/action/insertOne"
 
 
 def mongodb_evaluate(parameter_set, verbose=False):
     """Evaluate a parameter set and save the results to MongoDB."""
+    t0 = time()
+    results = evaluate(parameter_set)
+    runtime = time() - t0
+    print(results)
     utc = datetime.utcnow()
     results = {
-        **evaluate(parameter_set),
+        **results,
         "session_id": session_id,
         "timestamp": utc.timestamp(),
         "date": str(utc),
+        "runtime": time() - t0,
+        "seed": SEED,
+        "num_samples": num_samples,
+        "num_repeats": num_repeats,
     }
     results.pop("data_dir")
     results.pop("util_dir")
@@ -105,6 +124,10 @@ def mongodb_evaluate(parameter_set, verbose=False):
     return results
 
 
+def mongodb_evaluate_batch(parameter_sets, verbose=False):
+    return [mongodb_evaluate(p, verbose=verbose) for p in parameter_sets]
+
+
 def chunks(lst, n):
     """Yield successive n-sized chunks from lst."""
     for i in range(0, len(lst), n):
@@ -115,22 +138,25 @@ parameter_batch_sets = list(chunks(parameter_sets, batch_size))
 
 # %% submission
 log_folder = "data/interim/particle_packing/%j"
-# walltime = 5 * batch_size
-walltime = 5
-# use `myallocation` command to see available partition/account combos
-partition = "notchpeak-freecycle"
-account = "sparks"
+walltime_min = int(round(((120 / 60) * batch_size) + 3))
+# use `myallocation` command to see available account/partition combos
+# account = "sparks"
+# partition = "kingspeak"
+account = "owner-guest"
+partition = "kingspeak-guest"
 executor = AutoExecutor(folder=log_folder)
 executor.update_parameters(
-    timeout_min=walltime,
+    timeout_min=walltime_min,
+    slurm_nodes=None,
     slurm_partition=partition,
-    slurm_cpus_per_task=1,
-    slurm_additional_parameters={"account": account},
+    # slurm_cpus_per_task=1,
+    slurm_additional_parameters={"ntasks": 1, "account": account},
 )
 
 # sbatch array
-# jobs = executor.map_array(evaluate_batch, parameter_batch_sets)
-jobs = executor.map_array(mongodb_evaluate, parameter_sets)
+jobs = executor.map_array(mongodb_evaluate_batch, parameter_batch_sets)
+# jobs = executor.map_array(mongodb_evaluate, parameter_sets)
+print("Submitted jobs")
 # job_ids = [job.job_id for job in jobs]
 # # https://www.hpc2n.umu.se/documentation/batchsystem/job-dependencies
 # job_ids_str = ":".join(job_ids)  # e.g. "3937257_0:3937257_1:..."
