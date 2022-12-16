9bf94b3442d0fb0e455c092cc462946d4283f502
sgbaird <sterling.baird@utah.edu>
2022-12-15 22:57:50 -0700

    Create particle_packing_test.py

---
+++ tests/particle_packing_test.py
@@ -0,0 +1,22 @@
+from matsci_opt_benchmarks.particle_packing.utils.packing_generation import (
+    particle_packing_simulation,
+)
+from os import path
+
+
+def test_particle_packing_simulation():
+    results = particle_packing_simulation(
+        [0.5, 0.6, 0.7],
+        [0.1, 0.2, 0.3],
+        [0.25, 0.25, 0.5],
+        util_dir=path.join("src", "matsci_opt_benchmarks", "particle_packing", "utils"),
+        data_dir=path.join("data", "interim", "particle_packing"),
+    )
+    print(results)
+
+
+if __name__ == "__main__":
+    test_particle_packing_simulation()
+
+
+1 + 1
